6ca5f96aa9cdc72abc2af5b0ca7c7bfb19cd7505
Andreas <Andreas.Zwander@gmail.com>
2025-08-11 00:20:22 +0200

    change to a better name

--- CsvTools/Values/CsvValue.cs
+++ CsvTools/Values/CsvValue.cs
@@ -4,13 +4,20 @@ namespace CsvTools.Values;
 
 public readonly record struct CsvValue < T >
 {
-    public CsvCreationContext< T > Context { get; init; }
-    public InvariantValue< T > CurrentValue { get; init; }
-    public string? OriginalString => Context.OriginalString;
-    public InvariantValue< T > InitialValue => Context.InitialValue;
-    public bool IsModified => !EqualityComparer< InvariantValue< T > >.Default.Equals ( InitialValue , CurrentValue );
-    public CsvValue< T > WithNewValue ( T? newValue ) { return this with { CurrentValue = new InvariantValue< T > ( newValue ) }; }
-    public CsvValue< T > ResetToOriginal() { return this with { CurrentValue = InitialValue }; }
-    public string? ToString ( IFormatProvider cultureInfo ) { return string.Format ( cultureInfo , "{0}" , CurrentValue.Value ); }
-    public override string? ToString() { return ToString ( CultureInfo.InvariantCulture ); }
+    public CsvCreationContext< T > Original { get; init; }
+    public InvariantValue< T > Current { get; init; }
+    public string? ToString ( IFormatProvider cultureInfo ) => string.Format ( cultureInfo , "{0}" , Current.Value );
+    public override string? ToString() => ToString ( CultureInfo.InvariantCulture );
+}
+
+//public bool IsModified => !EqualityComparer< InvariantValue< T > >.Default.Equals ( InitialValue , CurrentValue );
+//public CsvValue< T > WithNewValue ( T? newValue ) { return this with { CurrentValue = new InvariantValue< T > ( newValue ) }; }
+//public CsvValue< T > ResetToOriginal() { return this with { CurrentValue = InitialValue }; }
+
+
+public static class CsvValueExtensions
+{
+    //public static CsvValue< T > WithNewValue< T > ( this CsvValue< T > value , T? newValue ) { return value.WithNewValue ( newValue ); }
+    //public string? OriginalString => Context.OriginalString;
+    //public InvariantValue< T > InitialValue => Context.InitialValue;
 }
