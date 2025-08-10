6a35e4614b1970f85f79183994660ea04d4a17c0
Andreas <Andreas.Zwander@gmail.com>
2025-08-10 19:51:03 +0200

    Add type safety to CsvValue

--- CsvTools/Values/CsvValue.cs
+++ CsvTools/Values/CsvValue.cs
@@ -1,26 +1,56 @@
-﻿namespace CsvTools.Values;
+﻿using System.Globalization;
+
+namespace CsvTools.Values;
+
+public readonly record struct InvariantValue < T > ( T? Value );
+public readonly record struct CsvCreationContext < T > ( string? OriginalString , InvariantValue< T > InitialValue );
 
 public readonly record struct CsvValue < T >
 {
     public CsvCreationContext< T > Context { get; init; }
-    public T? CurrentValue { get; init; }
+    public InvariantValue< T > CurrentValue { get; init; }
     public string? OriginalString => Context.OriginalString;
-    public T? InitialValue => Context.InitialValue;
-    public bool IsModified => !EqualityComparer< T >.Default.Equals ( InitialValue , CurrentValue );
+    public InvariantValue< T > InitialValue => Context.InitialValue;
+    public bool IsModified => !EqualityComparer< InvariantValue< T > >.Default.Equals ( InitialValue , CurrentValue );
+    public CsvValue< T > WithNewValue ( T? newValue ) { return this with { CurrentValue = new InvariantValue< T > ( newValue ) }; }
+    public CsvValue< T > ResetToOriginal() { return this with { CurrentValue = InitialValue }; }
+    public string? ToString ( IFormatProvider cultureInfo ) { return string.Format ( cultureInfo , "{0}" , CurrentValue.Value ); }
+    public override string? ToString() { return ToString ( CultureInfo.InvariantCulture ); }
+}
 
-    private CsvValue ( CsvCreationContext< T > context , T? currentValue )
+public class CsvValueBuilder < T > ( string? originalString )
+{
+    private CultureInfo _culture = CultureInfo.InvariantCulture;
+
+    public CsvValueBuilder< T > WithCulture ( CultureInfo culture )
     {
-        Context = context;
-        CurrentValue = currentValue;
+        _culture = culture;
+
+        return this;
     }
 
-    public static CsvValue< T > CreateInitial ( string? originalString , T? parsedValue )
+    public CsvValue< T > Build()
     {
-        var context = new CsvCreationContext< T > ( originalString , parsedValue );
+        var parsedValue = ParseString ( originalString , _culture );
+        var invariantInitial = new InvariantValue< T > ( parsedValue );
+        var context = new CsvCreationContext< T > ( originalString , invariantInitial );
 
-        return new CsvValue< T > ( context , parsedValue );
+        return new CsvValue< T > { Context = context , CurrentValue = invariantInitial };
     }
 
-    public CsvValue< T > WithNewValue ( T? newValue ) { return new CsvValue< T > ( Context , newValue ); }
-    public CsvValue< T > ResetToOriginal() { return new CsvValue< T > ( Context , Context.InitialValue ); }
+    private static T? ParseString ( string? s , CultureInfo c )
+    {
+        if ( s is null ) {
+            return default;
+        }
+
+        try {
+            var converter = System.ComponentModel.TypeDescriptor.GetConverter ( typeof ( T ) );
+
+            if ( converter.CanConvertFrom ( typeof ( string ) ) ) { return (T?) converter.ConvertFromString ( null , c , s ); }
+
+            return (T?) Convert.ChangeType ( s , typeof ( T ) , c );
+        }
+        catch ( Exception ) { return default; }
+    }
 }
