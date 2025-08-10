2acf8ff805337402a5a1a48d8903d2cf18483cab
Andreas <Andreas.Zwander@gmail.com>
2025-08-10 20:07:12 +0200

    Create first Test => Happy Path

---
+++ UnitTestCsvTools/CsvValueBuilderTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using CsvTools.Builders;
+
+namespace UnitTestCsvTools;
+
+public class CsvValueBuilderTests
+{
+    [ Fact ]
+    public void Build_WithGermanCulture_ShouldParseDecimalCorrectly()
+    {
+        const string inputString = "1.234,56";
+        var germanCulture = new CultureInfo ( "de-DE" );
+        const decimal expectedValue = 1234.56m;
+        var builder = new CsvValueBuilder< decimal > ( inputString );
+        var csvValue = builder.WithCulture ( germanCulture ).Build();
+        Assert.False ( csvValue.IsModified );
+        Assert.Equal ( expectedValue , csvValue.CurrentValue.Value );
+        Assert.Equal ( inputString , csvValue.OriginalString );
+    }
+}
