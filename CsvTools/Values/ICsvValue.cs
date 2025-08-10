da6bef2675ee60898e241087cce1fc01d00ccf22
Andreas <Andreas.Zwander@gmail.com>
2025-08-10 14:26:53 +0200

    Create ICsvValue

---
+++ CsvTools/Values/ICsvValue.cs
@@ -0,0 +1,9 @@
+﻿namespace CsvTools.Values;
+
+public interface ICsvValue
+{
+    string? OriginalString { get; }
+    object? InitialValue { get; }
+    object? CurrentValue { get; }
+    bool IsModified { get; }
+}
