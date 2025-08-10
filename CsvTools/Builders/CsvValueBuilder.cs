c8b663f2f38889e333f37ed5b417dd2ef435c4f3
Andreas <Andreas.Zwander@gmail.com>
2025-08-10 19:53:46 +0200

    create CsvValueBuilder.cs (refactoring)

---
+++ CsvTools/Builders/CsvValueBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace CsvTools.Values;
+
+public class CsvValueBuilder < T > ( string? originalString )
+{
+    private CultureInfo _culture = CultureInfo.InvariantCulture;
+
+    public CsvValueBuilder< T > WithCulture ( CultureInfo culture )
+    {
+        _culture = culture;
+
+        return this;
+    }
+
+    public CsvValue< T > Build()
+    {
+        var parsedValue = ParseString ( originalString , _culture );
+        var invariantInitial = new InvariantValue< T > ( parsedValue );
+        var context = new CsvCreationContext< T > ( originalString , invariantInitial );
+
+        return new CsvValue< T > { Context = context , CurrentValue = invariantInitial };
+    }
+
+    private static T? ParseString ( string? s , CultureInfo c )
+    {
+        if ( s is null ) {
+            return default;
+        }
+
+        try {
+            var converter = System.ComponentModel.TypeDescriptor.GetConverter ( typeof ( T ) );
+
+            if ( converter.CanConvertFrom ( typeof ( string ) ) ) { return (T?) converter.ConvertFromString ( null , c , s ); }
+
+            return (T?) Convert.ChangeType ( s , typeof ( T ) , c );
+        }
+        catch ( Exception ) { return default; }
+    }
+}
