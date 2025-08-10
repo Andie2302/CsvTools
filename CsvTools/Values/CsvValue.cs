9c4d75600b32b1e239252bc8a694ab30bce21ae3
Andreas <Andreas.Zwander@gmail.com>
2025-08-10 15:55:16 +0200

    Change HasValue and IsModified

--- CsvTools/Values/CsvValue.cs
+++ CsvTools/Values/CsvValue.cs
@@ -23,12 +23,12 @@ public readonly record struct CsvValue < T > : ICsvValue< T >
         OriginalString = originalString;
         InitialValue = initialValue;
         CurrentValue = currentValue;
-        IsModified = !EqualityComparer< T >.Default.Equals ( initialValue , currentValue );
+        IsModified = !object.Equals(initialValue, currentValue);
     }
 
     public static CsvValue< T > CreateInitial ( string? originalString , T? parsedValue ) { return new CsvValue< T > ( originalString , parsedValue , parsedValue ); }
     public CsvValue< T > WithNewValue ( T? newValue ) { return new CsvValue< T > ( OriginalString , InitialValue , newValue ); }
     public CsvValue< T > ResetToOriginal() { return new CsvValue< T > ( OriginalString , InitialValue , InitialValue ); }
-    public bool HasValue ( T? value ) { return EqualityComparer< T >.Default.Equals ( CurrentValue , value ); }
+    public bool HasValue(T? value) { return object.Equals(CurrentValue, value); }
     public override string ToString() { return CurrentValue?.ToString() ?? string.Empty; }
 }
