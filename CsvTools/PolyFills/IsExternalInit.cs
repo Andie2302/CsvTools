cc3a2bf5196c73e36c646d8c6ff135f79db16fbd
Andreas <Andreas.Zwander@gmail.com>
2025-08-11 03:37:43 +0200

    Create IsExternalInit.cs

--- CsvTools/PolyFills/IsExternalInit.cs
+++ CsvTools/PolyFills/IsExternalInit.cs
@@ -1,6 +1,6 @@
 ﻿// ReSharper disable CheckNamespace
 // The namespace must be: namespace System.Runtime.CompilerServices
-// This Code enable use of init-only Propertier in .net < 5  
+// This Code enable use of init-only Propertier in .net < 5
 #if !NET5_0_OR_GREATER
 namespace System.Runtime.CompilerServices
 {
