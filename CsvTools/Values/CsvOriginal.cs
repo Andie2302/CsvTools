9afb1e7e9721c4069e09cb135a3026e4f0a45e6c
Andreas <Andreas.Zwander@gmail.com>
2025-08-11 03:36:39 +0200

    Rename to CsvOriginal

--- CsvTools/Values/CsvOriginal.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace CsvTools.Values;
-
-public readonly record struct CsvOriginal < T > ( string? String , Invariant< T > Value );
---
+++ CsvTools/Values/CsvOriginalValues.cs
@@ -0,0 +1,3 @@
+﻿namespace CsvTools.Values;
+
+public readonly record struct CsvOriginalValues < T > ( string? String , CsvInvariantValue< T > Value );
